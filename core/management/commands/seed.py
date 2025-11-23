7a90d98ac2d75f5ac383fe9d35a2bc808d764ade
adith-p <adithprakash008@gmail.com>
2025-11-23 12:00:23 +0530

    feat(seed_file): add django subcommnd for seeding db

---
+++ core/management/commands/seed.py
@@ -0,0 +1,31 @@
+from django.core.management import BaseCommand
+from user.models import User
+from core.models import Movie, Show
+from faker import Faker
+from datetime import datetime, timedelta
+import random
+
+
+class Command(BaseCommand):
+    help = "management command to seed the db"
+
+    def handle(self, *args, **options):
+        faker = Faker()
+        admin_instance = User.objects.create(
+            is_superuser=True, username=faker.user_name(), password="admin"
+        )
+        movie_instance = Movie.objects.create(
+            title="movie1", duration_minutes=timedelta(minutes=30)
+        )
+
+        screen_name = ["sr1", "sr2", "sr3"]
+        # create shows
+        for _ in range(5):
+            Show.objects.create(
+                movie=movie_instance,
+                screen_name=random.choice(screen_name),
+                date_time=datetime.now(),
+                total_seat=random.randint(5, 20),
+            )
+
+        self.stdout.write(self.style.SUCCESS("Successfully seeded data!"))
